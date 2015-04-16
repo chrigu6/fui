4f9ace3b436ef15e338b0173f555ee0035e1d6bb
Nicolas Spycher <nic.spycher@gmail.com>
2015-04-16 14:53:00 +0200

    Added ability to add books, added ability to open books in pdf viewer

---
+++ Application/Virtual Library/Virtual Library/Books_2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Books
+{
+    private String name;
+    private String author;
+    private int year;
+    private String path;
+    private List<String> tags = new List<string>();
+
+    public Books(String name, String author, int year, String path)
+    {
+        this.name = name;
+        this.author = author;
+        this.year = year;
+        this.path = path;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public void setAuthor(String author)
+    {
+        this.author = author;
+    }
+
+    public String getAuthor()
+    {
+        return this.author;
+    }
+
+    public void setYear(int year)
+    {
+        this.year = year;
+    }
+
+    public int getYear()
+    {
+        return this.year;
+    }
+
+    public void setPath(String path)
+    {
+        this.path = path;
+    }
+
+    public String getPath()
+    {
+        return this.path;
+    }
+
+    public void setTag(String tag)
+    {
+        if (!this.tags.Contains(tag))
+        {
+            this.tags.Add(tag);
+        }
+    }
+
+    public List<String> getTags()
+    {
+        return this.tags;
+    }
+}
---
+++ Application/Virtual Library/Virtual Library/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Virtual_Library
+{
+    public partial class Form1 : Form
+    {
+        List<Books> library = new List<Books>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeLibrary();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach(Books book in library)
+            {
+                System.Diagnostics.Process.Start(book.getPath());
+            }
+        }
+
+        private void textBox1_MouseClick(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void InitializeLibrary()
+        {
+            Books book1 = new Books("A History of Mathematics", "Florian Cajori", 2010, @"\\psf\Dropbox\Unibe\3. Semester\FUI\Project\Application\Virtual Library\Virtual Library\books\A History of Mathematics.pdf");
+            book1.setTag("history");
+            book1.setTag("mathematics");
+            library.Add(book1);
+            Books book2 = new Books("A Short Account of the History of Mathematics", "W. W. Rouse Ball", 2010, @"\\psf\Dropbox\Unibe\3. Semester\FUI\Project\Application\Virtual Library\Virtual Library\books\A Short Account of the History of Mathematics.pdf");
+            book2.setTag("history");
+            book2.setTag("mathematics");
+            library.Add(book2);
+            Books book3 = new Books("First Six Books of the Elements of Euclid", "John Casey", 2007, @"\\psf\Dropbox\Unibe\3. Semester\FUI\Project\Application\Virtual Library\Virtual Library\books\First Six Books of the Elements of Euclid.pdf");
+            book3.setTag("elements");
+            book3.setTag("mathematics");
+            book3.setTag("euclid");
+            library.Add(book3);
+        }
+    }
+}
